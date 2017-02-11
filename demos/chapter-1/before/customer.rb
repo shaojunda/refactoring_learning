46d5c2deb49aaddb5fed9864921d11316bbbee42
shaojunda <shaojunda@gmail.com>
2017-02-12 01:19:08 +0800

    CHG: move amount_for to rental class and rename as charge

--- demos/chapter-1/before/customer.rb
+++ demos/chapter-1/before/customer.rb
@@ -14,7 +14,7 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{name}\n"
     @rentals.each do |element|
-      this_amount = amount_for(element)
+      this_amount = element.charge
 
       # add frequent renter points
       frequent_renter_points += 1
@@ -32,17 +32,4 @@ class Customer
     result
   end
 
-  def amount_for(rental)
-    result = 0
-    case rental.movie.price_code
-    when Movie::REGULAR
-      result += 2
-      result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-    when Movie::NEW_RELEASE
-      result += rental.days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-    end
-  end
 end
