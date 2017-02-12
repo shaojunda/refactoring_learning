16e35e98b43a1474a21c8d8b724067ffd8b080b6
shaojunda <shaojunda@gmail.com>
2017-02-12 14:40:23 +0800

    CHG: extract total frequent renter points

--- demos/chapter-1/test.rb
+++ demos/chapter-1/test.rb
@@ -4,6 +4,9 @@ require_relative "customer"
 
 movie = Movie.new("X man", Movie::REGULAR)
 rental = Rental.new(movie, 3)
+movie1 = Movie.new("X man 2", Movie::REGULAR)
+rental1 = Rental.new(movie1, 1)
 customer = Customer.new("lisi")
 customer.add_rental(rental)
+customer.add_rental(rental1)
 puts "#{customer.statement}"
