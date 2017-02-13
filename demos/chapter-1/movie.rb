3500d3971ca91ce1b2598bf7e59e50dae843e521
shaojunda <shaojunda@gmail.com>
2017-02-13 08:58:14 +0800

    CHG: implement charge in childrens_price and new_release_price

--- demos/chapter-1/movie.rb
+++ demos/chapter-1/movie.rb
@@ -7,36 +7,17 @@ class Movie
   NEW_RELEASE = 1
   CHILDRENS = 2
   attr_reader :title
-  attr_reader :price_code
+  attr_writer :price
 
-  def initialize(title, the_price_code)
-    @title, self.price_code = title, the_price_code
-  end
-
-  def price_code=(value)
-    @price_code = value
-    @price = case price_code
-    when REGULAR; RegularPrice.new
-    when NEW_RELEASE; NewReleasePrice.new
-    when CHILDRENS; ChildrensPrice.new
-    end
+  def initialize(title, price)
+    @title, self.price = title, price
   end
 
   def charge(days_rented)
-    result = 0
-    case price_code
-    when Movie::REGULAR
-      return @price.charge(days_rented)
-    when Movie::NEW_RELEASE
-      result += days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    @price.charge(days_rented)
   end
 
   def frequent_renter_points(days_rented)
-    (price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2 : 1
+    @price.frequent_renter_points(days_rented)
   end
 end
--- demos/chapter-1/test.rb
+++ demos/chapter-1/test.rb
@@ -1,10 +1,12 @@
 require_relative "movie"
 require_relative "rental"
 require_relative "customer"
+require_relative "regular_price"
+require_relative "new_release_price"
 
-movie = Movie.new("X man", Movie::REGULAR)
+movie = Movie.new("X man", RegularPrice.new)
 rental = Rental.new(movie, 3)
-movie1 = Movie.new("X man 2", Movie::NEW_RELEASE)
+movie1 = Movie.new("X man 2", NewReleasePrice.new)
 rental1 = Rental.new(movie1, 1)
 customer = Customer.new("lisi")
 customer.add_rental(rental)
